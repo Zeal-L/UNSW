88eb9a2bbbd4cd127b9ee78ec257a031eece5245
Zeal <abc982210694@gmail.com>
2020-10-18 11:48:51 +1100

    Successfully encapsulated the function 1 and 2

--- COMP1151/ass1/paint.c
+++ COMP1151/ass1/paint.c
@@ -26,81 +26,47 @@ void displayCanvas(int canvas[N_ROWS][N_COLS]);
 // Clear the canvas by setting every pixel to be white.
 void clearCanvas(int canvas[N_ROWS][N_COLS]);
 // Draw Line
-void line_drawing(int canvas[N_ROWS][N_COLS], int start_row,int start_col,int end_row,int end_col, int new_shade); 
+void line_drawing(int canvas[N_ROWS][N_COLS], int start_row,int start_col,int end_row,int end_col, int new_shade, int command); 
 // Fill Rectangle
-void rectangle_filling(int canvas[N_ROWS][N_COLS], int *start_r,int *start_c,int *end_r,int *end_c, int new_shade); 
+void rectangle_filling(int canvas[N_ROWS][N_COLS], int *start_r,int *start_c,int *end_r,int *end_c, int new_shade, int command); 
 // Changes the value beyond the boundary to the maximum or minimum of the border
-void hold_boundary(int *start_r,int *start_c,int *end_r,int *end_c);
+void hold_boundary(int *start_r,int *start_c,int *end_r,int *end_c, int command);
 // Checking the location of the given command on the canvas
 int check_location(int start_row,int start_col,int end_row,int end_col);
 // If draw it from the bottom up then flip the start and end
-void flipping (int *cur_r, int *cur_c, int *start_r,int *start_c,int *end_r,int *end_c);
+void flipping (int *start_r,int *start_c,int *end_r,int *end_c);
 // Copy and Paste
 void copy_paste (int canvas[N_ROWS][N_COLS], int start_row, int start_col, int end_row, int end_col, int target_row, int target_col);
 
 
 int main(void) {
-    int canvas[N_ROWS][N_COLS];
+    int canvas[N_ROWS][N_COLS], macro_store[0][0];
     clearCanvas(canvas);
 
-    int command, start_row, start_col, end_row, end_col, new_shade = 0;
+    int command, start_row, start_col, end_row, end_col, new_shade = 0, num_commands = 0;;
     int *start_r = &start_row, *start_c = &start_col, *end_r= &end_row, *end_c = &end_col;
     
     while (scanf("%d", &command) == 1) {
 
         if (command == 1) { // Draw Line
             while (scanf("%d%d%d%d", &start_row, &start_col, &end_row, &end_col) == 4) {
-                if (check_location(start_row, start_col, end_row, end_col) == 0) {
-
-                    line_drawing(canvas, start_row, start_col, end_row, end_col, new_shade);
-
-                    displayCanvas(canvas);
-                    printf("\n");
-                    canvasGraph(canvas);
-                    printf("\n");
-
-                } else if (check_location(start_row, start_col, end_row, end_col) == 1) {
-                        
-                    hold_boundary(start_r, start_c, end_r, end_c);
-                    line_drawing(canvas, start_row, start_col, end_row, end_col, new_shade);
-                    
-                    displayCanvas(canvas);
-                    printf("\n");
-                    canvasGraph(canvas);
-                    printf("\n");
-                }
+                line_drawing(canvas, start_row, start_col, end_row, end_col, new_shade, command);
                 break;
             }
         } 
         if (command == 2) { // Fill Rectangle
             while (scanf("%d%d%d%d", &start_row, &start_col, &end_row, &end_col) == 4) {
-                if (check_location(start_row, start_col, end_row, end_col) == 0) {
-
-                    rectangle_filling(canvas, start_r, start_c, end_r, end_c, new_shade);
-                    
-                    displayCanvas(canvas);
-                    printf("\n");
-                    canvasGraph(canvas);
-                    printf("\n");
-
-                } else if (check_location(start_row, start_col, end_row, end_col) == 1) {
-                
-                    hold_boundary(start_r, start_c, end_r, end_c);
-                    rectangle_filling(canvas, start_r, start_c, end_r, end_c, new_shade);
-
-                    displayCanvas(canvas);
-                    printf("\n");
-                    canvasGraph(canvas);
-                    printf("\n");
-                }
+                rectangle_filling(canvas, start_r, start_c, end_r, end_c, new_shade, command);
                 break;
             }
             
         }
         if (command == 3) { // Change Shade
             int check_shade = 0;
-            scanf("%d", &check_shade);
-            if (check_shade >= 0 && check_shade <= 4) new_shade = check_shade;
+            while (scanf("%d", &check_shade) == 1) {
+                if (check_shade >= 0 && check_shade <= 4) new_shade = check_shade;
+                break;
+            }
         } 
         if (command == 4) { // Copy and Paste
             int target_row, target_col;
@@ -114,7 +80,38 @@ int main(void) {
 
                 break;
             }
-            
+        }
+        if (command == 5) { // Macro Record
+            while (scanf("%d", &num_commands) == 1) {
+                macro_store[num_commands][5];
+                for (int i = 0; i < num_commands; i++) {
+                    for (int j = 0; j <= 5; j++) {
+                        scanf("%d", &macro_store[i][j]);
+                    }
+                }
+                break;
+            }
+        }
+        if (command == 6) { // Macro Playback
+            int row_offset = 0, col_offset = 0;
+            while (scanf("%d%d", &row_offset, &col_offset) == 2) { 
+                if (num_commands != 0) { // Check if command 5 has been used
+                    if (row_offset == 0 && col_offset == 0) { // If there is no change, just execute the Macro
+                        for (int i = 0; i < num_commands; i++) {
+                            if (macro_store[i][0] == 1) { // Draw Line
+
+                            } else if (macro_store[i][0] == 2) { // Fill Rectangle
+
+                            }
+                        }
+
+                    } else { // // apply the changes
+                        
+                    }
+                }
+                break;
+            }
+        
         }
     }
     return 0;
@@ -200,28 +197,14 @@ void clearCanvas(int canvas[N_ROWS][N_COLS]) {
     }
 }
 
-//If draw it from the bottom up then flip the start and end, otherwise do nothing
-void flipping (int *cur_r, int *cur_c, int *start_r,int *start_c,int *end_r,int *end_c) {
-    *cur_r = *start_r;
-    *cur_c = *start_c;
-    if (*start_r > *end_r) { 
-        *cur_r = *end_r;
-        *cur_c = *end_c;
-        *end_r = *start_r;
-        *end_c = *start_c;
-        *start_r = *cur_r;
-        *start_c = *cur_c;
-    } 
-}
-
 //Draw Line
-void line_drawing(int canvas[N_ROWS][N_COLS], int start_row,int start_col,int end_row,int end_col, int new_shade) { 
+void line_drawing(int canvas[N_ROWS][N_COLS], int start_row,int start_col,int end_row,int end_col, int new_shade, int command) { 
 
-    int cur_row, cur_col;
-    int *cur_r = &cur_row, *cur_c = &cur_col, 
-    *start_r = &start_row, *start_c = &start_col, *end_r = &end_row, *end_c = &end_col;
-    flipping (cur_r, cur_c, start_r, start_c, end_r, end_c);
-
+    int *start_r = &start_row, *start_c = &start_col, *end_r = &end_row, *end_c = &end_col;
+    // if the given command both starts and ends outside the canvas, ignore it
+    if (check_location(start_row, start_col, end_row, end_col) == -1) return;
+    hold_boundary(start_r, start_c, end_r, end_c, command);
+    int cur_row = *start_r, cur_col = *start_c;
 
     // Draw only perfectly horizontal or vertical lines
     if (start_row == end_row || start_col == end_col) {
@@ -261,14 +244,20 @@ void line_drawing(int canvas[N_ROWS][N_COLS], int start_row,int start_col,int en
             counter++;
         }
     }
+    displayCanvas(canvas);
+    printf("\n");
+    canvasGraph(canvas);
+    printf("\n");
 }
 
 // Fill Rectangle
-void rectangle_filling(int canvas[N_ROWS][N_COLS], int *start_r,int *start_c,int *end_r,int *end_c, int new_shade) {
+void rectangle_filling(int canvas[N_ROWS][N_COLS], int *start_r,int *start_c,int *end_r,int *end_c, int new_shade, int command) {
 
-    int cur_row, cur_col;
-    int *cur_r = &cur_row, *cur_c = &cur_col;
-    flipping (cur_r, cur_c, start_r, start_c, end_r, end_c);
+    int start_row = *start_r, start_col = *start_c, end_row = *end_r, end_col = *end_c;
+    // if the given command both starts and ends outside the canvas, ignore it
+    if (check_location(start_row, start_col, end_row, end_col) == -1) return;
+    hold_boundary(start_r, start_c, end_r, end_c, command);
+    int cur_row = *start_r, cur_col = *start_c;
 
     if (*start_r <= *end_r && *start_c <= *end_c) { // top-left to bottom-right
         while (cur_row <= *end_r) {
@@ -289,66 +278,85 @@ void rectangle_filling(int canvas[N_ROWS][N_COLS], int *start_r,int *start_c,int
             cur_row++;
         }
     }
+    displayCanvas(canvas);
+    printf("\n");
+    canvasGraph(canvas);
+    printf("\n");
+}
+
+//If draw it from the bottom up then flip the start and end, otherwise do nothing
+void flipping (int *start_r,int *start_c,int *end_r,int *end_c) {
+    int cur_r = *start_r;
+    int cur_c = *start_c;
+    if (*start_r > *end_r) { 
+        cur_r = *end_r;
+        cur_c = *end_c;
+        *end_r = *start_r;
+        *end_c = *start_c;
+        *start_r = cur_r;
+        *start_c = cur_c;
+    } 
 }
 
 //Changes the value beyond the boundary to the maximum or minimum of the border
-void hold_boundary(int *start_r,int *start_c,int *end_r,int *end_c) {
+void hold_boundary(int *start_r,int *start_c,int *end_r,int *end_c, int command) {
 
-    int cur_row, cur_col;
-    int *cur_r = &cur_row, *cur_c = &cur_col;
-    flipping (cur_r, cur_c, start_r, start_c, end_r, end_c);
+    flipping (start_r, start_c, end_r, end_c);
 
-    // Boundary for Diagonals, Shorten both the protruding rows and columns
-    int temp_start_row = *start_r, temp_start_col = *start_c, temp_end_row = *end_r, temp_end_col = *end_c;
-    if (*start_r - *end_r == *start_c - *end_c) { // 45° Diagonals ---- top-left to bottom-right
-        if (*end_r > N_ROWS-1) { // Lower right corner protruding
-            while ((*end_r > N_ROWS-1) || (*end_c > N_COLS-1)) {
-                temp_end_row--;
-                temp_end_col--;
-                *end_r = temp_end_row;
-                *end_c = temp_end_col;
+    // Checking if it is 45° Diagonals
+    if ((*start_r - *end_r == *start_c - *end_c) || (*start_r - *end_r == -1 * (*start_c - *end_c))) {
+        // Boundary for Diagonals, Shorten both the protruding rows and columns
+        int temp_start_row = *start_r, temp_start_col = *start_c, temp_end_row = *end_r, temp_end_col = *end_c;
+        if (*start_r - *end_r == *start_c - *end_c) { // 45° Diagonals ---- top-left to bottom-right
+            if (*end_r > N_ROWS-1) { // Lower right corner protruding
+                while ((*end_r > N_ROWS-1) || (*end_c > N_COLS-1)) {
+                    temp_end_row--;
+                    temp_end_col--;
+                    *end_r = temp_end_row;
+                    *end_c = temp_end_col;
+                }
+            } else if (*start_r < 0) { // Top left corner protrusion
+                while ((*start_r < 0) || (*start_c < 0)) {
+                    temp_start_row++;
+                    temp_start_col++;
+                    *start_r = temp_start_row;
+                    *start_c = temp_start_col;
+                }
             }
-        } else if (*start_r < 0) { // Top left corner protrusion
-            while ((*start_r < 0) || (*start_c < 0)) {
-                temp_start_row++;
-                temp_start_col++;
-                *start_r = temp_start_row;
-                *start_c = temp_start_col;
+        } else if (*start_r - *end_r == -1 * (*start_c - *end_c)) { // 45° Diagonals ---- top-right to bottom-left
+            if (*end_r > N_ROWS-1) { // Lower left corner protruding
+                while ((*end_r > N_ROWS-1) || (*end_c < 0)) {
+                    temp_end_row--;
+                    temp_end_col++;
+                    *end_r = temp_end_row;
+                    *end_c = temp_end_col;
+                }
+            } else if (*start_r < 0) { // Top right corner protruding
+                while ((*start_r < 0) || (*start_c > N_COLS-1)) {
+                    temp_start_row++;
+                    temp_start_col--;
+                    *start_r = temp_start_row;
+                    *start_c = temp_start_col;
+                }
             }
         }
-    } else if (*start_r - *end_r == -1 * (*start_c - *end_c)) { // 45° Diagonals ---- top-right to bottom-left
-        if (*end_r > N_ROWS-1) { // Lower left corner protruding
-            while ((*end_r > N_ROWS-1) || (*end_c < 0)) {
-                temp_end_row--;
-                temp_end_col++;
-                *end_r = temp_end_row;
-                *end_c = temp_end_col;
-            }
-        } else if (*start_r < 0) { // Top right corner protruding
-            while ((*start_r < 0) || (*start_c > N_COLS-1)) {
-                temp_start_row++;
-                temp_start_col--;
-                *start_r = temp_start_row;
-                *start_c = temp_start_col;
-            }
-        }
+    } else if ((*start_r == *end_r || *start_c == *end_c) || (command == 2)) {
+        // Normal boundary
+        if (*start_r < 0)          *start_r = 0;
+        if (*start_r > N_ROWS)     *start_r = N_ROWS-1;
+        if (*start_c < 0)          *start_c = 0;
+        if (*start_c > N_COLS)     *start_c = N_COLS-1;
+        if (*end_r > N_ROWS)       *end_r = N_ROWS-1;
+        if (*end_r < 0)            *end_r = 0;
+        if (*end_c > N_COLS)       *end_c = N_COLS-1;
+        if (*end_c < 0)            *end_c = 0;
     }
-
-    // Normal boundary
-    if (*start_r < 0)          *start_r = 0;
-    if (*start_r > N_ROWS)     *start_r = N_ROWS-1;
-    if (*start_c < 0)          *start_c = 0;
-    if (*start_c > N_COLS)     *start_c = N_COLS-1;
-    if (*end_r > N_ROWS)       *end_r = N_ROWS-1;
-    if (*end_r < 0)            *end_r = 0;
-    if (*end_c > N_COLS)       *end_c = N_COLS-1;
-    if (*end_c < 0)            *end_c = 0;
 }
 
 // Checking the location of the given command on the canvas
 int check_location(int start_row, int start_col, int end_row, int end_col) {
     
-    // if the given command both starts and ends outside the canvas, ignore it
+    // if the given command both starts and ends outside the canvas, ignore it, return -1
     if ((start_row >= 0 && start_row < N_ROWS) && 
         (start_col >= 0 && start_col < N_COLS) && 
           (end_row >= 0 && end_row < N_ROWS) &&
@@ -359,6 +367,6 @@ int check_location(int start_row, int start_col, int end_row, int end_col) {
     if (((start_row >= 0 && start_row < N_ROWS) && (start_col >= 0 && start_col < N_COLS)) ||
             ((end_row >= 0 && end_row < N_ROWS) && (end_col >= 0 && end_col < N_COLS))) {
         return 1;
-    }     
+    }
     return -1;       
 }

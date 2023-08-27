ce7b995bf7e6a8e3a513d891d9b7b6dea470d9b1
Zeal-L <abc982210694@gmail.com>
2023-08-27 16:11:35 +1000

    Getting ready for outbound links

--- COMP6771/X-Zone/src/X-Zone.cpp
+++ COMP6771/X-Zone/src/X-Zone.cpp
@@ -1,45 +1,6 @@
-#include <forward_list>
-#include <type_traits>
-#include <unordered_set>
-#include <algorithm>
-#include <compare>
-#include <cstdint>
-#include <exception>
-#include <functional>
-#include <iostream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <map>
-#include <memory>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <tuple>
-#include <typeindex>
-#include <typeinfo>
-#include <utility>
-#include <vector>
-using namespace std;
+#include "X-Zone.h"
 
-
-constexpr auto foo(const auto &f) -> decltype(auto) {
-	if constexpr (sizeof(f) != sizeof(void *)) {
-		auto val = f;
-		return val;
-	} else {
-		auto val = *f;
-		return val;
-	}
-}
 
 int main() {
-	constexpr int arr[3] = {};
-	auto var = foo(arr);
-
-	constexpr int arr2[3] = {};
-	auto x = arr2;
+	printf("Hello World!\n");
 }
-
-
---
+++ COMP6771/X-Zone/src/X-Zone.h
@@ -0,0 +1,30 @@
+#ifndef X_ZONE_H
+#define X_ZONE_H
+
+#include <forward_list>
+#include <type_traits>
+#include <unordered_set>
+#include <algorithm>
+#include <compare>
+#include <cstdint>
+#include <exception>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <typeindex>
+#include <typeinfo>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#endif // X_ZONE_H
